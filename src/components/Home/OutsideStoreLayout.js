5125ace4a2a4aa98012552266a7f14fa84765749
anxigy <gy010806@gmail.com>
2020-09-09 11:01:14 +0900

    가맹점 외부 페이지 1차 구현 (코드정리필요)

---
+++ src/components/Home/OutsideStoreLayout.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/native';
+import Theme from '../../theme';
+
+export const HomeHeaderIcon = styled.Image`
+  width: 22;
+  height: 22;
+  margin-left: 15;
+`;
+
+export const HomeSectionTitle = styled.Text`
+  font-size: 15;
+  font-weight: bold;
+`;
+
+export const HomeSectionSubTitle = styled.Text`
+  font-size: 11;
+  font-weight: bold;
+  color: ${Theme.colors.black}
+`;
