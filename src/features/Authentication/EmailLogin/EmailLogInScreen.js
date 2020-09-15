7d388093535c777bb2dc50673e0a639c488dd414
woojong92 <woojong92@gmail.com>
2020-09-15 10:28:00 +0900

    logInSlice AsyncThunk 적용

--- src/features/Authentication/EmailLogin/EmailLogInScreen.js
+++ src/features/Authentication/EmailLogin/EmailLogInScreen.js
@@ -22,6 +22,7 @@ import StyledTextInput from '../../../components/Authentication/StyledTextInput'
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { initialize, changeField } from './emailLogInSlice';
+import { setAccessToken } from '../authenticationSlice';
 
 EmailLogInScreen.propTypes = {
   navigation: PropTypes.object,
@@ -132,7 +133,7 @@ function EmailLogInScreen({ navigation }) {
 
       <NextButton
         style={{ position: 'absolute' }}
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => dispatch(setAccessToken())}
         isOk={email !== '' && password != ''}
       />
     </>
