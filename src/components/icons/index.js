2ad65d35ee4af87b2e3c136aa55a50783176cb04
woojong92 <woojong92@gmail.com>
2020-09-17 15:32:35 +0900

    DrawerContent 기본 ui 1차 구현 (resolve #84)

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,52 +2,30 @@ import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
 
 import theme from '../../theme';
 const { colors } = theme;
 
-EyeIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-EyeOffIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-ChevronDownIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-ChevronForwardIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-LocationPinIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-HeartIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-EarthIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-TimeIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
-PhoneIcon.propTypes = {
+const IconPropTypes = {
   size: PropTypes.number,
   color: PropTypes.string,
 };
 
-BackIcon.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
+EyeIcon.propTypes = IconPropTypes;
+EyeOffIcon.propTypes = IconPropTypes;
+ChevronDownIcon.propTypes = IconPropTypes;
+ChevronForwardIcon.propTypes = IconPropTypes;
+LocationPinIcon.propTypes = IconPropTypes;
+HeartIcon.propTypes = IconPropTypes;
+EarthIcon.propTypes = IconPropTypes;
+TimeIcon.propTypes = IconPropTypes;
+PhoneIcon.propTypes = IconPropTypes;
+BackIcon.propTypes = IconPropTypes;
+CloseIcon.propTypes = IconPropTypes;
+CouponIcon.propTypes = IconPropTypes;
 
 export function EyeIcon({ size = 25, color = colors.secondary }) {
   return <Ionicons name="eye" size={size} color={color} />;
@@ -55,14 +33,6 @@ export function EyeIcon({ size = 25, color = colors.secondary }) {
 
 export function EyeOffIcon({ size = 25, color = colors.secondary }) {
   return <Ionicons name="eye-off" size={size} color={color} />;
-}
-
-export function ChevronDownIcon({ size = 25, color = colors.darkGray }) {
-  return <Ionicons name="chevron-down" size={size} color={color} />;
-}
-
-export function ChevronForwardIcon({ size = 25, color = colors.darkGray }) {
-  return <Ionicons name="chevron-forward" size={size} color={color} />;
 }
 
 export function LocationPinIcon({ size = 25, color = colors.darkGray }) {
@@ -85,6 +55,32 @@ export function PhoneIcon({ size = 25, color = colors.primary }) {
   return <SimpleLineIcons name="phone" size={size} color={color} />;
 }
 
-export function BackIcon({ size = 25, color = colors.black }) {
+export function ChevronDownIcon({ size = 25, color = colors.darkGray }) {
+  return <Ionicons name="chevron-down" size={size} color={color} />;
+}
+
+export function ChevronForwardIcon({ size = 25, color = colors.darkGray }) {
+  return <Ionicons name="chevron-forward" size={size} color={color} />;
+}
+
+export function BackIcon({ size = 25, color = colors.darkGray }) {
   return <Ionicons name="chevron-back-outline" size={size} color={color} />;
+}
+
+export function CloseIcon({ size = 25, color = colors.black }) {
+  return <Ionicons name="close-outline" size={size} color={color} />;
+}
+
+export function CouponIcon({ size = 25, color = colors.black }) {
+  return <FontAwesomeIcons name="ticket" size={size} color={color} />;
+}
+
+export function PointIcon({ size = 25, color = colors.black }) {
+  return (
+    <MaterialCommunityIcons
+      name="car-brake-parking"
+      size={size}
+      color={color}
+    />
+  );
 }
